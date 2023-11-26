8ab442aeec5a7c739c929c1cc9bd8001789686ae
tomita_kazuki <tmtkzk@icloud.com>
2023-11-26 23:59:09 +0900

    形が無いverでバグが発生しているコード

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import axios from 'axios'
 import './App.css'
+import { useState } from 'react'
+import { Todo } from './Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([])
+
+
+
   const onClickFetchData = () => {
     axios.get('https://jsonplaceholder.typicode.com/todos')
       .then((res) => {
-        console.log(res)
+        setTodos(res.data);
       })
       .catch((err) => {
         console.log(err)
@@ -17,6 +23,9 @@ export default function App() {
     <>
       <div className="App">
         <button onClick={onClickFetchData}>データ取得</button>
+        {todos.map((todo) => (
+        < Todo title={todo.title} userId={todo.userId} />
+        ))}
       </div>
     </>
   )
