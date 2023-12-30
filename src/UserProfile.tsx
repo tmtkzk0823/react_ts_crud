5ce563c55391ba7caea43e01ce6e38a7a4209e65
tomita_kazuki <tmtkzk@icloud.com>
2023-12-30 10:39:37 +0900

    プロフィールのコンポーネントを作成

---
+++ src/UserProfile.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+export const UserProfile: FC<> = (props) => {
+return (
+  <>
+    <dl>
+      <dt>名前</dt>
+      <dd></dd>
+      <dt>趣味
+      </dt>
+      <dd></dd>
+    </dl>
+  </>
+  )
+}
