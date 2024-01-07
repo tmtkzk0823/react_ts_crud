b802feafce267969c84b63b32acbaa15c02ac5c9
tomita_kazuki <tmtkzk@icloud.com>
2024-01-07 23:36:06 +0900

    ユーザプロフィール用の型、jsxwo

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,23 @@
+import { UserProfile } from "../types/userProfile";
+
+import { FC } from "react";
+
+type Props = {
+  user: UserProfile
+}
+
+export const UserCard: FC<Props> = (props) => {
+  const { user } = props;
+  return (
+    <div>
+      <dl>
+        <dt>名前</dt>
+        <dd>{user.name}</dd>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>住所</dt>
+        <dd>{user.address}</dd>
+      </dl>
+    </div>
+  );
+}
