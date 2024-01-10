e271f2a109e1e280141a6931cc595f24d853fa96
tomita_kazuki <tmtkzk@icloud.com>
2024-01-10 09:04:25 +0900

    add_card_styleを当てる

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,10 +6,17 @@ type Props = {
   user: UserProfile
 }
 
+const style = {
+  border: "solid 1px #ccc",
+  borderRadius: "8px",
+  padding: "0 16px",
+  margin: "8px"
+};
+
 export const UserCard: FC<Props> = (props) => {
   const { user } = props;
   return (
-    <div>
+    <div style={style}>
       <dl>
         <dt>名前</dt>
         <dd>{user.name}</dd>
