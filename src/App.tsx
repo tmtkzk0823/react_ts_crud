99480e28e63f852accaa036b1af8519bd5949fba
tomita_kazuki <tmtkzk@icloud.com>
2024-01-24 10:12:13 +0900

    hooks途中

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,42 @@ import { useState } from 'react'
 //types
 import { TodoType } from './types/todo'
 import { User } from './types/user'
+import { ApiUsers } from './apis/types/userCard'
 
 //componnets
 import { UserProfile } from './UserProfile'
 import { UserCard } from './components/UserCard'
 import { Todo } from './Todo'
 import { Text } from './Text'
+import { UserStatus } from './types/userProfile'
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([])
+  const [users, setUsers] = useState<Array<UserStatus>>([])
 
   const user : User = {
     name: 'tommy',
     //hobbies: ['バドミントン', 'キャンプ']
   }
 
-   const userCard = {
+  const userCard = {
     id: 1,
     name: 'tommy',
     email: 'example.com',
     address: 'tokyo'
-   }
+  }
   const onClickFetchData = () => {
     axios.get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setTodos(res.data);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const onClickFetchUser = () => {
+    axios.get<Array<ApiUsers>>('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
         setTodos(res.data);
       })
@@ -54,6 +67,7 @@ export default function App() {
           completed={todo.completed}
         />
         ))}
+        <button onClick={onClickFetchUser}>データ取得</button>
         <UserCard user={userCard}/>
       </div>
     </>
